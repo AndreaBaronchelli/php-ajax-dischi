9a644646560cc5f92155fcc24cefb0fefff161cf
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-05-21 18:14:50 +0200

    bonus

--- vue/js/main.js
+++ vue/js/main.js
@@ -3,15 +3,38 @@ const app = new Vue({
     data: {
         discs: [],
         databaseAPI: window.location.href + "scripts/get_albums.php",
+        filterText: "All",
+        authors: [],
     },
     created() {
         axios
             .get(this.databaseAPI)
             .then((res) => {
                 this.discs = res.data;
+                res.data.forEach((el) => {
+                    if (!this.authors.includes(el.author)) {
+                        this.authors.push(el.author);
+                    }
+                });
             })
             .catch((err) => {
                 console.log(err);
             });
+    },
+    methods: {
+        filtering(value) {
+            axios
+                .get(this.databaseAPI + "?query" + value, {
+                    params: {
+                        query: value,
+                    },
+                })
+                .then((res) => {
+                    this.discs = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
     },
 });
--- vue/scripts/get_albums.php
+++ vue/scripts/get_albums.php
@@ -2,11 +2,23 @@
 // Import DB
 require_once __DIR__ . '/../partials/database.php';
 
-// var_dump($database);
+// FILTERING
+$filter = [];
+
+if (empty($_GET['query']) || $_GET['query'] === 'All') {
+    $filter = $database;
+} else {
+    foreach($database as $value) {
+
+        if ($_GET['query'] === $value['author']) {
+            $filter[] = $value;
+        }
+    }
+};
 
 // Conversion
 header('Content-Type: application/json');
 
-echo json_encode($database);
+echo json_encode($filter);
 
 
